1b9cfc478f2cbf353ce1ed4b3c300b470af0696d
Manas Malhotra <42417319+Manasmalhotra@users.noreply.github.com>
2023-09-17 22:29:38 +0530

    Delete src directory

--- src/main/java/com/example/courseapi/repositories/CourseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.courseapi.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.courseapi.models.Course;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CourseRepository extends JpaRepository<Course,Integer> {
-}
